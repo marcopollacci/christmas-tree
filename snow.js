abfa711cbefca5488f5e9fd65606762cf1315120
Marco Pollacci <marco.pollacci@antreem.it>
2024-11-20 19:20:18 +0100

    feat(tree): :tada: oh oh oh  🎄✨

---
+++ snow.js
@@ -0,0 +1,42 @@
+function letItSnow() {
+  for (let i = 0; i < 500; i++) {
+    const snowflake = document.createElement("span");
+    const size = randomize(0.15, 0.85);
+    const leftPos = randomize(0, 100);
+
+    snowflake.classList.add("snowflake");
+    snowflake.classList.add(`c${randomize(1, 3, true)}`);
+    snowflake.style.left = `${leftPos}%`;
+    snowflake.style.width = `${size}vw`;
+    snowflake.style.height = `${size}vw`;
+    snowflake.style.setProperty("--drift", `${leftPos + 2}%`);
+    snowflake.style.animationDelay = `${randomize(0, 100)}s`;
+    document.querySelector(".snow-area").appendChild(snowflake);
+  }
+}
+
+/**
+ * Returns a random number between min (inclusive) and max (inclusive)
+ * @param {number} min
+ * @param {number} max
+ * @param {boolean} [round=false] - If true, Math.round is called on the result.
+ * @returns {number}
+ */
+function randomize(min, max, round = false) {
+  const randomPick = Math.random() * (max - min) + min;
+  return round ? Math.round(randomPick) : randomPick;
+}
+
+/**
+ * Calculates the animation delay for a given snowflake based on its index.
+ * @param {number} flakeIndex - The index of the snowflake in the list of all snowflakes.
+ * @param {number} totalSnowCount - The total number of snowflakes.
+ * @returns {number} The delay for the snowflake's animation, in seconds.
+ */
+function calcAnimationDelay(flakeIndex, totalSnowCount) {
+  return flakeIndex < totalSnowCount * 0.1
+    ? randomize(0, 0.5)
+    : randomize(0.35, 18);
+}
+
+window.addEventListener("DOMContentLoaded", letItSnow);
